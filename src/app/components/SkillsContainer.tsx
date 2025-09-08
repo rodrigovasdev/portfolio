bffc999a9c217e1f6a3677476e32b1f5ec203e67
rodrigovasdev <146775218+rodrigovasdev@users.noreply.github.com>
2025-09-08 00:41:36 -0300

    Update SkillsContainer component to adjust list item font size for improved readability

--- src/app/components/SkillsContainer.tsx
+++ src/app/components/SkillsContainer.tsx
@@ -1,7 +1,7 @@
 
 export default function SkillsContainer ({ skills, isOdd }: { skills: string[]; isOdd: boolean }) {
 
-    const liClass = `text-lg text-center mx-auto ${isOdd ? "text-blue-500" : "text-purple-500"}`;
+    const liClass = `text-md text-center mx-auto ${isOdd ? "text-blue-500" : "text-purple-500"}`;
     return (
         <div id="skills" className="h-full w-full">
             <ul className="list-none flex flex-col md:flex-row gap-4 border-1 border-gray-400 py-0.5 px-0.5 rounded-xl">
