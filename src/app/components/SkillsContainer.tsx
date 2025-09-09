1d755c57cf126f0e8483d1e6c28fa03f679afa02
rodrigovasdev <146775218+rodrigovasdev@users.noreply.github.com>
2025-09-09 18:46:35 -0300

    Adjust font weight of list items in SkillsContainer component for improved emphasis

--- src/app/components/SkillsContainer.tsx
+++ src/app/components/SkillsContainer.tsx
@@ -1,7 +1,7 @@
 
 export default function SkillsContainer ({ skills, isOdd }: { skills: string[]; isOdd: boolean }) {
 
-    const liClass = `text-xs sm:text-sm md:text-lg font-stretch-extra-condensed md:font-stretch-extra-expanded xl:text-2xl text-center my-auto mx-auto ${isOdd ? "text-blue-500" : "text-purple-500"}`;
+    const liClass = `text-xs xl:font-semibold sm:text-sm md:text-lg font-stretch-extra-condensed md:font-stretch-extra-expanded xl:text-2xl text-center my-auto mx-auto ${isOdd ? "text-blue-500" : "text-purple-500"}`;
     return (
         <div id="skills" className="h-full w-full">
             <ul className="list-none flex gap-2 sm:gap-4 border-none md:border-1 border-gray-400 rounded-full py-2 px-5">
