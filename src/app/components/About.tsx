0335809544e2478b627da6c13e574e970bc5508c
rodrigovasdev <146775218+rodrigovasdev@users.noreply.github.com>
2025-09-09 18:41:47 -0300

    Adjust font size of headings in About component for improved readability

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -6,8 +6,8 @@ export default function About () {
             <div className="basis-3/5">
                 <div className="w-full md:w-3/4 grid gap-6 justify-center">
                     <div className="flex mb-10 gap-4 justify-center">
-                        <h1 className="text-7xl tracking-tight font-extrabold text-gray-900 text-center">About</h1>
-                        <h1 className="text-7xl tracking-tight font-extrabold text-purple-500"><b>me</b></h1>
+                        <h1 className="text-5xl sm:text-7xl tracking-tight font-extrabold text-gray-900 text-center">About</h1>
+                        <h1 className="text-5xl sm:text-7xl tracking-tight font-extrabold text-purple-500"><b>me</b></h1>
                     </div>
                     <span>Web developer and Software engineer, constantly evolving and currently seeking to enhance and continue advancing my professional career as a developer. Within web development, I work with technologies like Django, React, Next.js, Tailwind, Node.js, among others that you can see on my profile.</span>
                     <span>I am eager to join a development team to deepen my knowledge in software development. I am seeking opportunities that allow me to continue learning while contributing my skills as a full-stack developer. I am proactive and highly motivated, with a strong passion for learning new technologies and continuously growing professionally.</span>
