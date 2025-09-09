23922ffb53e364fc0e1a9ad2ff7786bdfae6c974
rodrigovasdev <146775218+rodrigovasdev@users.noreply.github.com>
2025-09-09 18:38:30 -0300

    Adjust gap spacing in SkillsContainer component for improved layout

--- src/app/components/SkillsContainer.tsx
+++ src/app/components/SkillsContainer.tsx
@@ -1,10 +1,10 @@
 
 export default function SkillsContainer ({ skills, isOdd }: { skills: string[]; isOdd: boolean }) {
 
-    const liClass = `text-xs sm:text-sm md:text-lg  font-stretch-extra-condensed md:font-stretch-extra-expanded xl:text-2xl text-center my-auto mx-auto ${isOdd ? "text-blue-500" : "text-purple-500"}`;
+    const liClass = `text-xs sm:text-sm md:text-lg font-stretch-extra-condensed md:font-stretch-extra-expanded xl:text-2xl text-center my-auto mx-auto ${isOdd ? "text-blue-500" : "text-purple-500"}`;
     return (
         <div id="skills" className="h-full w-full">
-            <ul className="list-none flex gap-4 border-none md:border-1 border-gray-400 rounded-full py-2 px-5">
+            <ul className="list-none flex gap-2 sm:gap-4 border-none md:border-1 border-gray-400 rounded-full py-2 px-5">
                 {skills.map((skill, index) => (
                     <li key={index} className={liClass}>{skill}</li>
                 ))}
