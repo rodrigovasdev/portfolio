09565053c0c598d3b68728fdd01603321f302c43
rodrigovasdev <146775218+rodrigovasdev@users.noreply.github.com>
2025-09-10 00:07:54 -0300

    Refactor skills array in CAREER_DATA for consistency and clarity

--- src/app/components/SkillsContainer.tsx
+++ src/app/components/SkillsContainer.tsx
@@ -1,9 +1,9 @@
 
 export default function SkillsContainer ({ skills, isOdd }: { skills: string[]; isOdd: boolean }) {
 
-    const liClass = `text-xs xl:font-semibold sm:text-sm md:text-lg font-stretch-extra-condensed md:font-stretch-extra-expanded xl:text-2xl text-center my-auto mx-auto ${isOdd ? "text-blue-500" : "text-purple-500"}`;
+    const liClass = `text-xs xl:font-semibold sm:text-sm md:text-lg font-stretch-extra-condensed md:font-stretch-expanded xl:text-2xl text-center my-auto mx-auto ${isOdd ? "text-blue-500" : "text-purple-500"}`;
     return (
-        <div id="skills" className="h-full w-full">
+        <div id="skills" className="h-full w-full mx-auto">
             <ul className="list-none flex gap-2 sm:gap-4 border-none md:border-1 border-gray-400 rounded-full py-2 px-5">
                 {skills.map((skill, index) => (
                     <li key={index} className={liClass}>{skill}</li>
--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -24,7 +24,7 @@ export const CAREER_DATA: CareerData[] = [
         description: "As a junior software developer, I was responsible for modifying key user interfaces for the business using Svelte and Tailwind, optimizing user experience and enhancing platform usability. For design and prototyping of the interfaces, I utilized Figma, ensuring a seamless transition from design to implementation. Additionally, I implemented new endpoints in the API using Django Rest Framework, ensuring efficient and secure integration with the frontend, which contributed to the system's scalability and performance.",
         date: "Dic 2024 - Mar 2025",
         longDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        skills: ["SaaS", "Svelte", "Tailwind CSS", "Figma", "UI/UX Design"],
+        skills: ["SaaS", "Svelte", "Tailwind", "Figma", "UI/UX"],
         images: [
         {
             original: "/career/foundaxis/1.webp",
@@ -44,7 +44,7 @@ export const CAREER_DATA: CareerData[] = [
         description: "I taught full-stack web development, covering key technologies such as Python, Django, JavaScript, CSS, and HTML. I was responsible for guiding students in building complete web applications, from the frontend to the backend. This role has allowed me to strengthen my communication skills, especially when explaining and demonstrating software or scripts, ensuring that technical concepts are accessible and understandable for all students. I also highlight that learning and adapting to new technologies has helped me strengthen my knowledge.",
         date: "Jul 2024 - Jan 2025",
         longDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        skills: ["Python", "Django", "JavaScript", "CSS", "HTML", "Teaching"],
+        skills: ["Python", "Django", "CSS", "HTML", "Teaching"],
         images: [
         {
             original: "/career/edutecno/1.webp",
@@ -61,7 +61,7 @@ export const CAREER_DATA: CareerData[] = [
         description: "A web app I built for sharing high-resolution photos, developed with Django, React, Next.js, Tailwind CSS, and AWS.",
         date: "Jan 2025",
         longDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        skills: ["Django", "React", "Next.js", "Tailwind CSS", "AWS"],
+        skills: ["Django", "React", "Next.js", "Tailwind", "AWS"],
         images: [
         {
             original: "/career/photosgram/1.webp",
